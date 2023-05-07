8603ac442da3f06337bb25e78c73393fbd984444
Muhammad Faiq Khan <76833898+MFaiqKhan@users.noreply.github.com>
2023-05-07 13:22:19 +0500

    Update Q5.py

--- Lab04/Q5.py
+++ Lab04/Q5.py
@@ -1,139 +1,89 @@
 """
 To convert the VaccumAgent to a model-based reflex agent, you need to maintain an internal state to keep track of the cleanliness status of each room. You can then update the agent's act method to consider the internal state while making decisions
 """
-
-import time
-from com.environment import Environment
-from com.environment import Room
-from com.agent import Agent
-
-class NRoomVaccumCleanerEnvironment(Environment.Environment):
-    def __init__(self, agent, rooms=2):
-        self.rooms = rooms
-        self.rooms = [Room.Room(i, 'dirty') for i in range(1, rooms+1)]
-        self.agent = agent
-        self.currentRoom = self.rooms[0]
-        self.delay(1000)
+class ModelBasedReflexVaccumAgent():
+    def __init__(self, model):
+        self.model = model
+        self.currentRoom = 1
+        self.actions = ['clean', 'right', 'left']
         self.step = 1
-        self.action = ""
-        self.score = 0  # Initialize the score
 
-    def executeStep(self, n=1):
-        for _ in range(0, n):
-            self.displayPerception()
-            res = self.agent.act()
-            self.action = res
+    def updateModel(self, location, status):
+        for room in self.model:
+            if room['location'] == location:
+                room['status'] = status
 
-            if res == "clean":
-                if self.currentRoom.status == "dirty":
-                    self.score += 25  # Add 25 points for cleaning a dirty room
-                    print("Adding 25 points: Cleaned a dirty room")
-                self.currentRoom.status = "clean"
-            elif res == "right":
-                self.score -= 1  # Subtract 1 point for moving from a room
-                print("Subtracting 1 point: Moved from a room")
-                if self.currentRoom == self.rooms[-1]:
-                    self.currentRoom = self.rooms[0]
-                else:
-                    self.currentRoom = self.rooms[self.rooms.index(self.currentRoom) + 1]
-            elif res == "left":
-                self.score -= 1  # Subtract 1 point for moving from a room
-                print("Subtracting 1 point: Moved from a room")
-                if self.currentRoom == self.rooms[0]:
-                    self.currentRoom = self.rooms[-1]
-                else:
-                    self.currentRoom = self.rooms[self.rooms.index(self.currentRoom) - 1]
-
-            if self.currentRoom.status == "dirty":
-                self.score -= 10  # Subtract 10 points if the current room is dirty
-                print("Subtracting 10 points: Current room is dirty")
-
-            self.displayAction()
-            self.step += 1
-            time.sleep(self.delay_time / 1000)
-            self.displayScore()  # Display the score after each action
-
-    def executeAll(self):
-            ''' execute the environment until all rooms are clean '''
-            raise NotImplementedError('action must be defined!')
-        
     def displayPerception(self):
-            ''' display the current room and it's status '''
-            print(f"Perception at Step {self.step}:\nCurrent Room: {self.currentRoom.location}\nRoom Status: {self.currentRoom.status}")
-        
-    def displayAction(self):
-            print("------- Action taken at step %d is [%s]" %(self.step,self.action))
-        
-    def delay(self,n=1000):
-            ''' delay in milliseconds '''
-            self.delay_time = n  
-
-    def displayScore(self):
-        print(f"Agent's score at step {self.step}: {self.score}")
-        
-        
-        
-
-"""
-class ModelBasedReflexVacuumAgent(Agent.Agent):
-    def __init__(self):
-        self.previous_action = "clean"
+        ''' display the current room and its status '''
+        print(f"Perception at Step {self.step}:\nCurrent Room: {self.currentRoom}\nRoom Status: {self.model[self.currentRoom - 1]['status']}")
 
     def act(self):
-        if self.previous_action == "clean":
-            next_action = "right"
-        elif self.previous_action == "right":
-            next_action = "clean"
+        self.displayPerception()
+        ''' return the action to be taken based on the current percept '''
+        if self.model[self.currentRoom - 1]['status'] == 'dirty':
+            self.updateModel(self.currentRoom, 'clean')
+            action = 'clean'
         else:
-            next_action = "clean"
-
-        self.previous_action = next_action
-        return next_action
-    """
-
-class ModelBasedReflexVaccumAgent(Agent.Agent):
-    def __init__(self):
-        self.room_status = {}
-
-   # def sense(self, environment):
-    #    self.environment = environment  # store the environment
-      #  location = self.environment.currentRoom.location
-     #   status = self.environment.currentRoom.status
-       # self.room_status[location] = status
-
-    def act(self):
-        location = self.environment.currentRoom.location
-        status = self.environment.currentRoom.status
-
-        if status == "dirty":
-            return "clean"
-        else:
-            next_location = (location + 1) % len(self.environment.rooms)
-            if self.room_status.get(next_location, None) != "clean":
-                return "right"
+            self.updateModel(self.currentRoom, 'clean')
+            if self.currentRoom == len(self.model):
+                self.currentRoom = 1
+                action = 'left'
             else:
-                return "left"
-            
-     """
-     This agent will not work properly, as it cannot update its internal state based on the environment's actual state. 
-     The agent's decisions will be based on the initial, incomplete knowledge of the environment, which may not be accurate. 
-     Since the agent cannot perceive the environment, it cannot update its internal model, and its performance will degrade.
-     In conclusion, taking the sensors away from a model-based reflex agent will prevent it from working effectively because it cannot maintain an accurate internal model of the environment.
-     """
-            
+                self.currentRoom += 1
+                action = 'right'
+        self.step += 1
+        return action
 
-if __name__ == "__main__":
-    agent = ModelBasedReflexVaccumAgent()
-    environment = NRoomVaccumCleanerEnvironment(agent, 10)
-    environment.executeStep(10)
-    print(f"Agent's score: {environment.score}")
+# Test
+n_rooms = 5
+model = [{'location': i + 1, 'status': 'dirty'} for i in range(n_rooms)]
+modell = [{'location': 1, 'status': 'dirty'}, {'location': 2, 'status': 'clean'}, {'location': 3, 'status': 'dirty'}, {'location': 4, 'status': 'dirty'}, {'location': 5, 'status': 'dirty'}]
+agent = ModelBasedReflexVaccumAgent(modell)
+for i in range(10):
+    print(agent.act())
+
+
+# Converting the agent to the model-based reflex agent and took out the sensors and perceptions away from the agent.
+# The agent now has an internal model of the environment and uses it to decide its actions.
+# # Does your agent still work fine? Explain why.    
+
+""" 
+The provided code does not rely on any sensors or perceptions. It uses the internal model of the agent to 
+determine the state of each room and decide its actions. 
+The agent's model is pre-populated with room status information and gets updated as the agent performs actions.
+The code will work fine in this specific scenario since it's a simple, 
+deterministic environment where the agent only needs to clean the rooms in a linear sequence. 
+However, in more complex or dynamic environments, the lack of sensors or 
+perceptions would limit the agent's ability to adapt to changes or uncertainties.
+In those cases, incorporating sensor data into the agent's logic would be necessary to make 
+better decisions and adapt to the environment's changes. But for this simple vacuum cleaner example, 
+the code will work as intended without the need for sensors.
+"""
+
+
+
+
+# Explanation of the code
+"""
+Initialization: The agent initializes its internal model with n_rooms, where each room has a location and a 'dirty' status. 
+The agent starts in room 1 and sets its available actions to 'clean', 'right', and 'left'.
+Updating the model: The agent updates its internal model when it cleans a room or moves to another room.
+Displaying the perception: The agent displays the current room and its status at each step.
+Acting: The agent decides which action to take based on its internal model. 
+If the current room is dirty, it cleans the room. If the room is clean, 
+it moves to the next room (right) or returns to the first room (left) if it's the last room.
+To test the agent, create an instance with the desired number of 
+rooms and execute the act() method for a certain number of iterations:
+This code snippet represents a simple, model-based reflex agent that does not use sensors to perceive its environment. 
+Instead, it relies on its internal model to make decisions and update room statuses.
+"""
+
     
     
-    
-  """ Is a full observable
+""" Is a full observable
   A simple reflex agent acts only based on the current percept, ignoring the percept history. 
   In the context of the vacuum cleaner problem, a simple reflex agent would take actions based on the current location and the dirt status of the room it's in. 
-  """
+"""
 
 """
 A model-based reflex agent, on the other hand, can handle partially observable environments by maintaining an internal state based on the percept history. 
